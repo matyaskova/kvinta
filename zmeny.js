d82a3b756236f4f40558347c2c52efaaf4b906c7
matyaskova <43047469+matyaskova@users.noreply.github.com>
2023-11-08 12:18:41 +0100

    Add files via upload

--- zmeny.js
+++ zmeny.js
@@ -1,4 +1,11 @@
 function zmena(element,barva) {
     element.style.color = barva
-    
+  }
+function zvetsi(element,size) {
+    element.style.fontSize = size
+}  
+
+function zarovka(objekt,on_or_off ){
+    objekt.src = on_or_off +".png"
+
 }
